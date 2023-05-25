d3ffd10d5f4327c0ce9d47e1e1f93a4d2d0303b8
Kirill Ivanov <bragov4ik@hotmail.com>
2023-05-25 23:29:01 +0300

    move network to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,13 @@
 use clap::Parser;
-use consensus::graph::GenesisPayload;
 use futures::StreamExt;
 use libp2p::mdns;
-use libp2p::swarm::{NetworkBehaviour, SwarmBuilder, SwarmEvent};
-use libp2p::{Multiaddr, PeerId};
-use protocol::request_response::SwarmRequestResponse;
-use tokio_util::sync::CancellationToken;
+use libp2p::swarm::SwarmEvent;
+use libp2p::Multiaddr;
 use tracing::{debug, error, info, warn};
 
-use rust_hashgraph::algorithm::datastructure::Graph;
-use signatures::Ed25519Signer;
 use std::error::Error;
-use std::time::Duration;
 
-use crate::consensus::graph::{EventPayload, GraphWrapper};
-use crate::data_memory::{DistributedDataMemory, MemoryBus};
-use crate::instruction_storage::InstructionMemory;
-use crate::module::ModuleChannelServer;
-use crate::processor::single_threaded::ShardProcessor;
-use crate::types::{Sid, Vid};
+use crate::network::CombinedBehaviourEvent;
 
 // "modules" as in `module::Module`
 mod behaviour;
@@ -31,6 +20,7 @@ mod encoding;
 mod io;
 mod logging_helpers;
 mod module;
+mod network;
 mod processor;
 mod protocol;
 mod signatures;
@@ -56,43 +46,10 @@ struct Args {
 
     #[clap(short, long)]
     dial_address: Option<String>,
-}
 
-#[derive(NetworkBehaviour)]
-#[behaviour(out_event = "CombinedBehaviourEvent")]
-struct CombinedBehaviour {
-    // Main logic
-    main: behaviour::Behaviour,
-    request_response: libp2p::request_response::Behaviour<SwarmRequestResponse>,
-    // MDNS performs LAN node discovery, allows not to manually write peer addresses
-    mdns: mdns::async_io::Behaviour,
-}
-
-#[derive(Debug)]
-// TODO: add Main event and check if applies
-#[allow(clippy::large_enum_variant)]
-enum CombinedBehaviourEvent {
-    Main(behaviour::ToSwarmEvent),
-    RequestResponse(request_response::Event),
-    Mdns(mdns::Event),
-}
-
-impl From<behaviour::ToSwarmEvent> for CombinedBehaviourEvent {
-    fn from(event: behaviour::ToSwarmEvent) -> Self {
-        CombinedBehaviourEvent::Main(event)
-    }
-}
-
-impl From<mdns::Event> for CombinedBehaviourEvent {
-    fn from(event: mdns::Event) -> Self {
-        CombinedBehaviourEvent::Mdns(event)
-    }
-}
-
-impl From<request_response::Event> for CombinedBehaviourEvent {
-    fn from(value: request_response::Event) -> Self {
-        CombinedBehaviourEvent::RequestResponse(value)
-    }
+    /// Seed to generate key
+    #[clap(long)]
+    key_seed: Option<u8>,
 }
 
 #[tokio::main]
@@ -113,98 +70,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let args = Args::parse();
 
-    let local_keypair = libp2p::identity::Keypair::generate_ed25519();
-    let local_ed25519_keypair = local_keypair
-        .clone()
-        .into_ed25519()
-        .expect("Just created this variant");
-    let local_peer_id = PeerId::from(local_keypair.public());
-    info!("Local peer id: {:?}", local_peer_id);
-
-    let transport = libp2p::development_transport(local_keypair).await?;
-
-    let mut join_handles = vec![];
-    let shutdown_token = CancellationToken::new();
-
-    // consensus
-    let signer = Ed25519Signer::new(local_ed25519_keypair.clone());
-    let graph = Graph::new(
-        local_peer_id,
-        EventPayload::<Vid, Sid>::new(vec![]),
-        GenesisPayload {
-            pubkey: local_ed25519_keypair.public().into(),
-        },
-        30,
-        signer,
-        (),
-    );
-    let consensus = GraphWrapper::from_graph(graph);
-    let (consensus_server, consensus_client) =
-        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
-    join_handles.push(tokio::spawn(consensus.run(consensus_server)));
-
-    // data memory
-    let (memory_bus_data_memory, memory_bus_processor) = MemoryBus::channel(CHANNEL_BUFFER_LIMIT);
-    let data_memory =
-        DistributedDataMemory::new(local_peer_id, memory_bus_data_memory, ENCODING_SETTINGS);
-    let (data_memory_server, data_memory_client) =
-        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
-    join_handles.push(tokio::spawn(data_memory.run(data_memory_server)));
-
-    // instruction memory
-    let instruction_memory =
-        InstructionMemory::new(ENCODING_SETTINGS.data_shards_sufficient.try_into().unwrap());
-    let (instruction_memory_server, instruction_memory_client) =
-        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
-    join_handles.push(tokio::spawn(
-        instruction_memory.run(instruction_memory_server),
-    ));
-
-    // processor
-    let processor = ShardProcessor::new(memory_bus_processor);
-    let (processor_server, processor_client) =
-        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
-    join_handles.push(tokio::spawn(processor.run(processor_server)));
-
-    let (behaviour_server, behaviour_client) =
-        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
-
-    let (mut request_response_server, request_response_client) =
-        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
-
-    let request_response = libp2p::request_response::Behaviour::new(
-        SwarmRequestResponse,
-        std::iter::once((
-            protocol::versions::RequestResponseVersion::V1,
-            libp2p_request_response::ProtocolSupport::Full,
-        )),
-        Default::default(),
-    );
-    let main_behaviour = behaviour::Behaviour::new(
-        local_peer_id,
-        Duration::from_millis(2000),
-        behaviour_server,
-        consensus_client,
-        instruction_memory_client,
-        data_memory_client,
-        processor_client,
-        request_response_client,
-    );
-    let mdns = mdns::async_io::Behaviour::new(Default::default(), local_peer_id)?;
-
-    let behaviour = CombinedBehaviour {
-        main: main_behaviour,
-        request_response,
-        mdns,
-    };
-
-    // We want the connection background tasks to be spawned
-    // onto the tokio runtime.
-    let mut swarm = SwarmBuilder::with_tokio_executor(transport, behaviour, local_peer_id).build();
-
-    // Tell the swarm to listen on all interfaces and a random, OS-assigned
-    // port.
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+    let (mut swarm, mut request_response_server, join_handles, shutdown_token) =
+        network::new(None).await.unwrap();
 
     // Dial the peer identified by the multi-address given as the second
     // command-line argument, if any.
@@ -214,32 +81,35 @@ async fn main() -> Result<(), Box<dyn Error>> {
         info!("Dialed {}", addr)
     }
 
-    // repl is sync, so run it in a separate thread
-    let cloned_shutdown = shutdown_token.clone();
-    std::thread::spawn(|| ui::run_repl(behaviour_client, cloned_shutdown));
-
     // todo: send sigterm
     loop {
         tokio::select! {
             event = swarm.select_next_some() => {
                 match event {
-                    SwarmEvent::NewListenAddr { address, .. } => info!("Listening on {:?}", address),
-                    SwarmEvent::Behaviour(CombinedBehaviourEvent::Mdns(
-                        mdns::Event::Discovered(list)
-                    )) => {
-                        for (peer, _) in list {
+                    SwarmEvent::Behaviour(CombinedBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                        for (peer, address) in list {
                             swarm.behaviour_mut().main.inject_peer_discovered(peer);
+                            swarm.behaviour_mut().request_response.add_address(&peer, address);
                         }
                     }
-                    SwarmEvent::Behaviour(CombinedBehaviourEvent::Mdns(
-                        mdns::Event::Expired(list)
-                    )) => {
-                        for (peer, _) in list {
+                    SwarmEvent::Behaviour(CombinedBehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
+                        for (peer, address) in list {
                             if !swarm.behaviour_mut().mdns.has_node(&peer) {
                                 swarm.behaviour_mut().main.inject_peer_expired(&peer);
+                                swarm.behaviour_mut().request_response.remove_address(&peer, &address);
                             }
                         }
                     }
+                    SwarmEvent::Behaviour(CombinedBehaviourEvent::RequestResponse(e)) => {
+                        let handle_result = request_response::handle_request_response_event(
+                            &mut request_response_server, e
+                        ).await;
+                        if let Err(_) = handle_result {
+                            error!("Shutting down...");
+                            shutdown_token.cancel();
+                            break;
+                        }
+                    },
                     SwarmEvent::Behaviour(CombinedBehaviourEvent::Main(Err(behaviour::Error::CancelSignal))) => {
                         info!("{}", behaviour::Error::CancelSignal);
                         shutdown_token.cancel();
@@ -250,15 +120,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         shutdown_token.cancel();
                         break;
                     },
-                    SwarmEvent::Behaviour(CombinedBehaviourEvent::RequestResponse(r)) => {
-                        let handle_result = request_response::handle_request_response_event(&mut request_response_server, r).await;
-                        if let Err(_) = handle_result {
-                            error!("Shutting down...");
-                            shutdown_token.cancel();
-                            break;
-                        }
-                    }
                     SwarmEvent::Behaviour(event) => info!("{:?}", event),
+                    SwarmEvent::NewListenAddr { address, .. } => {
+                        let local_peer_id = *swarm.local_peer_id();
+                        eprintln!(
+                            "Local node is listening on {:?}",
+                            address.with(libp2p::multiaddr::Protocol::P2p(local_peer_id.into()))
+                        );
+                    }
+                    SwarmEvent::IncomingConnection { .. } => {}
+                    SwarmEvent::ConnectionEstablished { .. } => {}
+                    SwarmEvent::ConnectionClosed { .. } => {}
+                    SwarmEvent::OutgoingConnectionError { .. } => {}
+                    SwarmEvent::IncomingConnectionError { .. } => {}
+                    SwarmEvent::Dialing(peer_id) => eprintln!("Dialing {peer_id}"),
                     other => debug!("{:?}", other),
                 }
             }
@@ -268,6 +143,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     shutdown_token.cancel();
                     break;
                 };
+                info!("{:?}", action);
                 match action {
                     request_response::InEvent::MakeRequest { request, to } => {
                         let request_id = swarm.behaviour_mut().request_response.send_request(&to, request.clone());
---
+++ src/network.rs
@@ -0,0 +1,178 @@
+use libp2p::mdns;
+use libp2p::{
+    identity,
+    swarm::{NetworkBehaviour, Swarm, SwarmBuilder},
+    PeerId,
+};
+use rust_hashgraph::algorithm::datastructure::Graph;
+use tokio_util::sync::CancellationToken;
+use tracing::info;
+
+use std::error::Error;
+use std::time::Duration;
+
+use crate::consensus::graph::{EventPayload, GenesisPayload, GraphWrapper};
+use crate::data_memory::{DistributedDataMemory, MemoryBus};
+use crate::instruction_storage::InstructionMemory;
+use crate::module::ModuleChannelServer;
+use crate::processor::single_threaded::ShardProcessor;
+use crate::protocol::request_response::SwarmRequestResponse;
+use crate::protocol::versions::RequestResponseVersion;
+use crate::signatures::Ed25519Signer;
+use crate::types::{Sid, Vid};
+use crate::{behaviour, ui, CHANNEL_BUFFER_LIMIT, ENCODING_SETTINGS};
+
+#[derive(NetworkBehaviour)]
+#[behaviour(out_event = "CombinedBehaviourEvent")]
+pub struct CombinedBehaviour {
+    // Main logic
+    pub main: behaviour::Behaviour,
+    pub request_response: libp2p::request_response::Behaviour<SwarmRequestResponse>,
+    // MDNS performs LAN node discovery, allows not to manually write peer addresses
+    pub mdns: mdns::async_io::Behaviour,
+}
+
+#[derive(Debug)]
+#[allow(clippy::large_enum_variant)]
+pub enum CombinedBehaviourEvent {
+    Main(behaviour::ToSwarmEvent),
+    RequestResponse(crate::request_response::Event),
+    Mdns(mdns::Event),
+}
+
+impl From<behaviour::ToSwarmEvent> for CombinedBehaviourEvent {
+    fn from(event: behaviour::ToSwarmEvent) -> Self {
+        CombinedBehaviourEvent::Main(event)
+    }
+}
+
+impl From<mdns::Event> for CombinedBehaviourEvent {
+    fn from(event: mdns::Event) -> Self {
+        CombinedBehaviourEvent::Mdns(event)
+    }
+}
+
+impl From<crate::request_response::Event> for CombinedBehaviourEvent {
+    fn from(value: crate::request_response::Event) -> Self {
+        CombinedBehaviourEvent::RequestResponse(value)
+    }
+}
+
+pub async fn new(
+    key_seed: Option<u8>,
+) -> Result<
+    (
+        Swarm<CombinedBehaviour>,
+        ModuleChannelServer<crate::request_response::Module>,
+        Vec<tokio::task::JoinHandle<()>>,
+        CancellationToken,
+    ),
+    Box<dyn Error>,
+> {
+    // Create a public/private key pair, either random or based on a seed.
+    let local_keypair = match key_seed {
+        Some(s) => {
+            let mut bytes = [0u8; 32];
+            bytes[0] = s;
+            identity::Keypair::ed25519_from_bytes(bytes).unwrap()
+        }
+        None => identity::Keypair::generate_ed25519(),
+    };
+    let local_ed25519_keypair = local_keypair
+        .clone()
+        .into_ed25519()
+        .expect("Just created this variant");
+    let local_peer_id = PeerId::from(local_keypair.public());
+    info!("Local peer id: {:?}", local_peer_id);
+
+    let transport = libp2p::development_transport(local_keypair).await?;
+
+    let mut join_handles = vec![];
+    let shutdown_token = CancellationToken::new();
+
+    // consensus
+    let signer = Ed25519Signer::new(local_ed25519_keypair.clone());
+    let graph = Graph::new(
+        local_peer_id,
+        EventPayload::<Vid, Sid>::new(vec![]),
+        GenesisPayload {
+            pubkey: local_ed25519_keypair.public().into(),
+        },
+        30,
+        signer,
+        (),
+    );
+    let consensus = GraphWrapper::from_graph(graph);
+    let (consensus_server, consensus_client) =
+        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
+    join_handles.push(tokio::spawn(consensus.run(consensus_server)));
+
+    // data memory
+    let (memory_bus_data_memory, memory_bus_processor) = MemoryBus::channel(CHANNEL_BUFFER_LIMIT);
+    let data_memory =
+        DistributedDataMemory::new(local_peer_id, memory_bus_data_memory, ENCODING_SETTINGS);
+    let (data_memory_server, data_memory_client) =
+        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
+    join_handles.push(tokio::spawn(data_memory.run(data_memory_server)));
+
+    // instruction memory
+    let instruction_memory =
+        InstructionMemory::new(ENCODING_SETTINGS.data_shards_sufficient.try_into().unwrap());
+    let (instruction_memory_server, instruction_memory_client) =
+        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
+    join_handles.push(tokio::spawn(
+        instruction_memory.run(instruction_memory_server),
+    ));
+
+    // processor
+    let processor = ShardProcessor::new(memory_bus_processor);
+    let (processor_server, processor_client) =
+        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
+    join_handles.push(tokio::spawn(processor.run(processor_server)));
+
+    let (behaviour_server, behaviour_client) =
+        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
+
+    let (request_response_server, request_response_client) =
+        ModuleChannelServer::new(None, CHANNEL_BUFFER_LIMIT, shutdown_token.clone());
+
+    let request_response = libp2p::request_response::Behaviour::new(
+        SwarmRequestResponse,
+        std::iter::once((
+            RequestResponseVersion::V1,
+            libp2p_request_response::ProtocolSupport::Full,
+        )),
+        Default::default(),
+    );
+    let main_behaviour = behaviour::Behaviour::new(
+        local_peer_id,
+        Duration::from_millis(2000),
+        behaviour_server,
+        consensus_client,
+        instruction_memory_client,
+        data_memory_client,
+        processor_client,
+        request_response_client,
+    );
+    let mdns = mdns::async_io::Behaviour::new(Default::default(), local_peer_id)?;
+
+    let behaviour = CombinedBehaviour {
+        main: main_behaviour,
+        request_response,
+        mdns,
+    };
+
+    // We want the connection background tasks to be spawned
+    // onto the tokio runtime.
+    let mut swarm = SwarmBuilder::with_tokio_executor(transport, behaviour, local_peer_id).build();
+
+    // Tell the swarm to listen on all interfaces and a random, OS-assigned
+    // port.
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // repl is sync, so run it in a separate thread
+    let cloned_shutdown = shutdown_token.clone();
+    std::thread::spawn(|| ui::run_repl(behaviour_client, cloned_shutdown));
+
+    Ok((swarm, request_response_server, join_handles, shutdown_token))
+}
