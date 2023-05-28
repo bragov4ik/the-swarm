25124216e8e9404ed8b68e93e710b8773e35524a
Kirill Ivanov <bragov4ik@hotmail.com>
2023-05-28 21:55:35 +0300

    retrieve recognized txs as well

--- src/consensus/graph.rs
+++ src/consensus/graph.rs
@@ -7,7 +7,7 @@ use futures::{Future, StreamExt};
 use libp2p::PeerId;
 use pin_project_lite::pin_project;
 use rust_hashgraph::algorithm::datastructure::{self, EventCreateError, Graph};
-use rust_hashgraph::algorithm::event::Hash;
+use rust_hashgraph::algorithm::event::{EventWrapper, Hash};
 use rust_hashgraph::algorithm::PushError;
 use rust_hashgraph::algorithm::{Clock, Signer};
 use serde::{Deserialize, Serialize};
@@ -122,12 +122,17 @@ pin_project! {
     /// behaviour thus uses [`ModuleChannelClient`]).
     ///
     /// Use [`Self::from_graph()`] to create, [`Self::run()`] to operate.
+    #[project = GraphWrapperProjection]
     pub struct GraphWrapper<TDataId, TShardId, TSigner, TClock> {
-        // todo: replace parentheses - ()
         inner: Graph<EventPayload<TDataId, TShardId>, GenesisPayload, PeerId, TSigner, TClock>,
+        // Notification for possible new finalized/recognized transactions
         state_updated: Arc<Notify>,
+        // transactions scheduled for inclusion into a next event
         included_transaction_buffer: Vec<Transaction<TDataId, TShardId, PeerId>>,
-        retrieved_transaction_buffer: (PeerId, VecDeque<Transaction<TDataId, TShardId, PeerId>>, Hash),
+        // transactions from latest finalized transaction
+        finalized_transaction_buffer: (PeerId, VecDeque<Transaction<TDataId, TShardId, PeerId>>, Hash),
+        // transactions from latest recognized (seen in partial order) event
+        recognized_transaction_buffer: (PeerId, VecDeque<Transaction<TDataId, TShardId, PeerId>>, Hash),
     }
 }
 impl<TDataId, TShardId, TSigner, TClock> GraphWrapper<TDataId, TShardId, TSigner, TClock> {
@@ -138,7 +143,12 @@ impl<TDataId, TShardId, TSigner, TClock> GraphWrapper<TDataId, TShardId, TSigner
             inner: graph,
             state_updated: Arc::new(Notify::new()),
             included_transaction_buffer: Vec::new(),
-            retrieved_transaction_buffer: (
+            finalized_transaction_buffer: (
+                PeerId::random(),
+                VecDeque::new(),
+                Hash::from_array([0; 64]),
+            ),
+            recognized_transaction_buffer: (
                 PeerId::random(),
                 VecDeque::new(),
                 Hash::from_array([0; 64]),
@@ -223,56 +233,114 @@ where
     }
 }
 
-impl<TDataId, TShardId, TSigner, TClock> Stream for GraphWrapper<TDataId, TShardId, TSigner, TClock>
+pub type NextTxData<TDataId, TShardId> = (PeerId, Transaction<TDataId, TShardId, PeerId>, Hash);
+
+pub enum NextTx<TDataId, TShardId> {
+    Recognized(NextTxData<TDataId, TShardId>),
+    Finalized(NextTxData<TDataId, TShardId>),
+}
+
+impl<TDataId, TShardId, TSigner, TClock> GraphWrapper<TDataId, TShardId, TSigner, TClock>
 where
-    TDataId: Serialize + Eq + std::hash::Hash + Debug + Clone,
-    TShardId: Serialize + Eq + std::hash::Hash + Debug + Clone,
+    TDataId: Serialize + Eq + std::hash::Hash + Debug + Clone + 'static,
+    TShardId: Serialize + Eq + std::hash::Hash + Debug + Clone + 'static,
     TSigner: Signer<GenesisPayload, SignerIdentity = PeerId>,
     TClock: Clock,
 {
-    type Item = (PeerId, Transaction<TDataId, TShardId, PeerId>, Hash);
-
-    fn poll_next(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> Poll<Option<Self::Item>> {
-        let this = self.project();
-
-        // wake up if something changed
-        let state_updated_notification = this.state_updated.notified();
-        pin!(state_updated_notification);
-        let _ = state_updated_notification.poll(cx);
-
+    // todo: replace `graph` with function
+    fn next_tx<'a, F>(
+        this: &mut Graph<EventPayload<TDataId, TShardId>, GenesisPayload, PeerId, TSigner, TClock>,
+        get_next_event: F,
+        tx_buffer: &mut (
+            PeerId,
+            VecDeque<Transaction<TDataId, TShardId, PeerId>>,
+            Hash,
+        ),
+    ) -> Option<NextTxData<TDataId, TShardId>>
+    where
+        F: for<'b> Fn(
+            &'b mut Graph<EventPayload<TDataId, TShardId>, GenesisPayload, PeerId, TSigner, TClock>,
+        ) -> Option<
+            &'b EventWrapper<EventPayload<TDataId, TShardId>, GenesisPayload, PeerId>,
+        >,
+    {
         loop {
-            if let Some(tx) = this.retrieved_transaction_buffer.1.pop_front() {
+            if let Some(tx) = tx_buffer.1.pop_front() {
                 // feed transactions from an event one by one
-                return Poll::Ready(Some((
-                    this.retrieved_transaction_buffer.0,
-                    tx,
-                    this.retrieved_transaction_buffer.2.clone(),
-                )));
+                return Some((tx_buffer.0, tx, tx_buffer.2.clone()));
             } else {
                 // no txs left in previous event, getting a new one
-                match this.inner.next_event() {
+                match get_next_event(this) {
                     Some(event) => {
                         let author = event.author().clone();
                         let mut txs: VecDeque<_> = event.payload().transactions.clone().into();
                         let next_tx = txs.pop_front();
-                        this.retrieved_transaction_buffer.0 = author;
-                        this.retrieved_transaction_buffer.1 = txs;
+                        tx_buffer.0 = author;
+                        tx_buffer.1 = txs;
                         match next_tx {
-                            Some(tx) => {
-                                return Poll::Ready(Some((author, tx, event.hash().clone())))
-                            }
+                            Some(tx) => return Some((author, tx, event.hash().clone())),
                             None => {
                                 // more events might be available
                                 continue;
                             }
                         }
                     }
-                    None => return Poll::Pending,
+                    None => return None,
                 }
             }
+        }
+    }
+
+    // If `None` then it might not be available yet
+    fn next_finalized(
+        this: &mut GraphWrapperProjection<'_, TDataId, TShardId, TSigner, TClock>,
+    ) -> Option<NextTx<TDataId, TShardId>> {
+        Self::next_tx(
+            this.inner,
+            |inner| inner.next_finalized_event(),
+            this.finalized_transaction_buffer,
+        )
+        .map(|d| NextTx::Finalized(d))
+    }
+
+    // If `None` then it might not be available yet
+    fn next_recognized(
+        this: &mut GraphWrapperProjection<'_, TDataId, TShardId, TSigner, TClock>,
+    ) -> Option<NextTx<TDataId, TShardId>> {
+        Self::next_tx(
+            this.inner,
+            |inner| inner.next_recognized_event(),
+            this.finalized_transaction_buffer,
+        )
+        .map(|d| NextTx::Recognized(d))
+    }
+}
+
+impl<TDataId, TShardId, TSigner, TClock> Stream for GraphWrapper<TDataId, TShardId, TSigner, TClock>
+where
+    TDataId: Serialize + Eq + std::hash::Hash + Debug + Clone + 'static,
+    TShardId: Serialize + Eq + std::hash::Hash + Debug + Clone + 'static,
+    TSigner: Signer<GenesisPayload, SignerIdentity = PeerId>,
+    TClock: Clock,
+{
+    type Item = NextTx<TDataId, TShardId>;
+
+    fn poll_next(
+        self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+
+        // wake up if something changed
+        {
+            let state_updated_notification = this.state_updated.notified();
+            pin!(state_updated_notification);
+            let _ = state_updated_notification.poll(cx);
+        }
+        match Self::next_finalized(&mut this).or_else(|| Self::next_recognized(&mut this)) {
+            // we're assuming infinite stream so no `None` is returned
+            Some(tx) => Poll::Ready(Some(tx)),
+            None => Poll::Pending,
         }
     }
 }
@@ -286,11 +354,15 @@ where
         loop {
             tokio::select! {
                 next_tx = self.next() => {
-                    let Some((from, tx, event_hash)) = next_tx else {
+                    let Some(next_tx) = next_tx else {
                         info!("stream of events ended, shuttung down consensus");
                         return;
                     };
-                    if let Err(_) = connection.output.send(OutEvent::FinalizedTransaction { from, tx, event_hash }).await {
+                    let out_event = match next_tx {
+                        NextTx::Recognized((from, tx, event_hash)) => OutEvent::RecognizedTransaction { from, tx, event_hash },
+                        NextTx::Finalized((from, tx, event_hash)) => OutEvent::FinalizedTransaction { from, tx, event_hash },
+                    };
+                    if let Err(_) = connection.output.send(out_event).await {
                         info!("`connection.output` is closed, shuttung down consensus");
                         return;
                     }
