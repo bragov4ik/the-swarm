e371976010bc993e4a7566b220f479107d5d7ecf
Kirill Ivanov <bragov4ik@hotmail.com>
2023-06-07 20:28:39 +0300

    uncomment pause

--- scenarios/run_and_suspend/run_and_suspend.sh
+++ scenarios/run_and_suspend/run_and_suspend.sh
@@ -10,6 +10,8 @@ sleep 1
 cd ./the-swarm
 path_to_script="./scenarios/run_and_suspend/_run_and_suspend.exp"
 "$path_to_script"
+echo "Cleaning up..."
+&> /dev/null
 cd ../
 docker compose -f ./the-swarm/docker-compose.yml --project-directory . down
 docker network rm the-swarm-test-net-1 || true
